9bdbb35bbe057741ca952b42f2a54c3d9537c92c
sushanth babu kasarla <sushanthbabukasarla@sushanths-MacBook-Pro.local>
2020-02-22 20:27:42 -0800

    Added new employee

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -16,6 +16,7 @@ public class EmployeeController {
     {
 		List<Employee> employeesList = new ArrayList<Employee>();
 		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
+		employeesList.add(new Employee(2,"Sushanth","Babu","sushi@hotmail.com"));
 		return employeesList;
     }
 

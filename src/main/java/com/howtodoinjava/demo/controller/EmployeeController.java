b8cbef35b3442c83d6c89d990b44c5e2cb9eba72
sushanth babu kasarla <sushanthbabukasarla@sushanths-MBP.attlocal.net>
2020-02-23 11:51:00 -0800

    Added krish

--- src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
+++ src/main/java/com/howtodoinjava/demo/controller/EmployeeController.java
@@ -17,7 +17,24 @@ public class EmployeeController {
 		List<Employee> employeesList = new ArrayList<Employee>();
 		employeesList.add(new Employee(1,"lokesh","gupta","howtodoinjava@gmail.com"));
 		employeesList.add(new Employee(2,"Sushanth","Babu Kasarla","sushi@hotmail.com"));
+		employeesList.add(new Employee(3,"Krish","Datla","kris@krish.com"));
 		return employeesList;
+    }
+	
+	@RequestMapping("/employee")
+    public Employee getEmployee() 
+    {
+		Employee employee = new Employee();
+		employee.setFirstName("Lathesh");
+		employee.setEmail("karkeralathesh@gmail.com");
+		employee.setLastName("Karkera");
+		return employee;
+    }
+	
+	@RequestMapping("/health")
+    public String getHealth() 
+    {
+		return "Am fine";
     }
 
 }
